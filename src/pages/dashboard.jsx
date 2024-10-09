175161f044b63d9b0e42be3c4ae294683ccb3191
geri35 <geri@upi.edu>
2024-10-09 19:52:34 +0700

    {layout, dashboard}

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -5,10 +5,47 @@ const Dashboard = () => {
   const location = useLocation();
   const { username } = location.state || {};
 
+  const cardData = [
+    {
+      icon: "/book-icon.svg", // URL gambar untuk Card 1
+      description: "Lihat Rules",
+      link: "#",
+    },
+    {
+      icon: "/test-icon.svg", // URL gambar untuk Card 2
+      description: "Mulai Kerjakan Quiz",
+      link: "#",
+    },
+    {
+      icon: "/logout-icon.svg", // URL gambar untuk Card 3
+      description: "Log Out",
+      link: "#",
+    },
+  ];
+
   return (
     <div className="bg-gradient-to-r from-purple-800 to-purple-500 min-h-screen font-[Poppins] text-[#0B0B0C] p-5">
       <div className="text-4xl font-bold">
-        Hi, {username ? username : "Pengguna"}, Selamat Datang
+        Hi, Selamat Datang {username ? username : "Pengguna"}
+      </div>
+      {/* menu */}
+      <div className="container mx-auto pt-5">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {cardData.map((card, index) => (
+            <a
+              key={index}
+              href={card.link}
+              className="bg-purple-300 p-5 rounded-lg shadow flex flex-col items-center"
+            >
+              <img
+                src={card.icon}
+                alt={card.description}
+                className="mb-2 h-16 w-16"
+              />
+              <p>{card.description}</p>
+            </a>
+          ))}
+        </div>
       </div>
     </div>
   );
